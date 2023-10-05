73fa26aa6c65aaddb907214533392e11ed92a16c
MohamedSayed9392 <MohamedSayed9392@gmail.com>
2023-10-05 03:03:29 +0300

    * Fix Unassigned dublicate send to transfer list

--- app/function-overrides/unassigned-list-override.js
+++ app/function-overrides/unassigned-list-override.js
@@ -4,18 +4,20 @@ export const unassignedListOverride = () => {
   const unassignedList = UTUnassignedItemsView.prototype.renderSection;
   UTUnassignedItemsView.prototype.renderSection = function(e, t, i, o, ...args) {
     const result = unassignedList.call(this, e, t, i, o, ...args);
-    this.sections.forEach(element => {
-      appendSectionPrices({
-        listRows: element.listRows.map(({ __root, __auction, data }) => ({
-          __root,
-          __auction,
-          data,
-        })),
-        headerElement: $(element._header.__root),
-        isRelistSupported: true,
-        sectionHeader: "Unassigned",
-      });
+    
+    var element = this.sections[t];
+
+    appendSectionPrices({
+      listRows: element.listRows.map(({ __root, __auction, data }) => ({
+        __root,
+        __auction,
+        data,
+      })),
+      headerElement: $(element._header.__root),
+      isRelistSupported: true,
+      sectionHeader: "Unassigned",
     });
+
     return result;
   };  
 };
